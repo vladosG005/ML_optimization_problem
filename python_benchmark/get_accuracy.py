343eda15da2d39b8f1822cb20a34ca4ece1511f4
vladosG005 <vladosG005@yandex.ru>
2026-04-28 20:44:42 +0300

    Update accuracy.py: Add loading dataset

--- python_benchmark/get_accuracy.py
+++ python_benchmark/get_accuracy.py
@@ -1,6 +1,8 @@
 from sys import argv
 import pickle
-filename = argv[1]
-with open(filename, 'rb') as file:
-    model = pickle.load(file)
-    print(type(model))
+import pandas as pd
+model_file = argv[1]
+dataset_file = argv[2]
+with open(model_file, 'rb') as f:
+    model = pickle.load(f)
+    pd.read_csv(dataset_file)
