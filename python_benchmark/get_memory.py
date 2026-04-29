1ed4a234df496e254bce4478706369e1ef8e9f35
vladosG005 <vladosG005@yandex.ru>
2026-04-29 13:35:48 +0300

    Update get_memory.py: improve readability

--- python_benchmark/get_memory.py
+++ python_benchmark/get_memory.py
@@ -3,4 +3,6 @@ from load_model import load_model
 import resource
 X, Y, model = load_model(argv[1], argv[2])
 model.predict(X)
-print((resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss + resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) / 1024)
+memory_children = resource.getrusage(resource.RUSAGE_CHILDREN)
+memory_self = resource.getrusage(resource.RUSAGE_SELF)
+print((memory_children.ru_maxrss + memory_self.ru_maxrss) / 1024)
