8ca605e85779eaf1d4c9a7b3c2f2e7765158ee86
vladosG005 <vladosG005@yandex.ru>
2026-04-29 10:33:56 +0300

    Update get_memory.py: measure memory

--- python_benchmark/get_memory.py
+++ python_benchmark/get_memory.py
@@ -1,3 +1,6 @@
 from sys import argv
 from load_model import load_model
+import resource
 X, Y, model = load_model(argv[1], argv[2])
+model.predict(X)
+print(resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss / 1024)
