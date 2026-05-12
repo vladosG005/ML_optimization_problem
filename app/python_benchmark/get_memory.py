a3d8e5c59cbabfa6d76d86bb6e90fa5b554da168
vladosG005 <vladosG005@yandex.ru>
2026-05-12 20:46:29 +0300

    Do proper xgboost time and accuracy measurements

--- app/python_benchmark/get_memory.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-from load_model import load_model
-import resource
-X, Y, model = load_model(argv[1], argv[2])
-model.predict(X)
-memory_children = resource.getrusage(resource.RUSAGE_CHILDREN)
-memory_self = resource.getrusage(resource.RUSAGE_SELF)
-print((memory_children.ru_maxrss + memory_self.ru_maxrss) / 1024)
